5baf95d6448fe0c164ac8c93d51cd184d685c1a4
Coltensiemer <colten50@hotmail.com>
2022-10-27 22:29:10 -0400

    Started

---
+++ js.js
@@ -0,0 +1,9 @@
+const mainContainer = document.getElementById('main-container'); 
+const thanksContainer = document.getElementById('thankyou-container')
+const submitButton = document.getElementById('submit'); 
+
+
+submitButton.addEventListener('click', () => { 
+    
+})
+
