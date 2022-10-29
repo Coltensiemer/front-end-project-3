5aa2642f2804b781d7fcce87a486a3920f5fa47a
Coltensiemer <colten50@hotmail.com>
2022-10-29 00:54:45 -0400

    Update

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
+"use strict"
+
 const mainContainer = document.getElementById('main-container'); 
 const thanksContainer = document.getElementById('thankyou-container')
 const submitButton = document.getElementById('submit'); 
-
+const rating = document.getElementById('button-rating'); 
+const rates= document.querySelectorAll('.button'); 
 
 submitButton.addEventListener('click', () => { 
-    
+    thanksContainer.classList.remove('hidden'); 
+    mainContainer.style.display= "none"; 
 })
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => { 
+        rating.innerHTML = rate.innerHTML;
+    } )
+    
+});
+
+
+
+
 
