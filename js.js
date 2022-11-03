3297b2f85af97f6d4be6d5709990448fcb74d94d
Coltensiemer <colten50@hotmail.com>
2022-11-02 21:06:28 -0400

    update

--- js.js
+++ js.js
@@ -5,16 +5,34 @@ const thanksContainer = document.getElementById('thankyou-container')
 const submitButton = document.getElementById('submit'); 
 const rating = document.querySelector('.button').value;
 const myRating = document.getElementById('rating'); 
-// const form = document.getElementById('buttons-container').value; 
+
+const form = document.getElementById('buttons-container'); 
 
 
 submitButton.addEventListener('click', () => { 
+
     thanksContainer.classList.remove('hidden'); 
     mainContainer.style.display= "none"; 
 
+   
 })
 
-myRating.textContent = ` You selected ${rating} out of 5`;
+form.addEventListener('submit', (e) => { 
+e.preventDefault(); 
+
+const data= new FormData()
+
+for(let value of data ){
+    console.log(value);
+
+}
+
+ 
+})
+
+
+
+
 
  
 
