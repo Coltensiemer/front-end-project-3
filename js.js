4e39a6c8acb0cddc06f3057be55bd501651bd19f
Colten Siemergit <colten50@hotmail.com>
2022-12-05 23:18:06 -0500

    Cleaning up challenge

--- js.js
+++ js.js
@@ -3,32 +3,30 @@
 const mainContainer = document.getElementById('main-container'); 
 const thanksContainer = document.getElementById('thankyou-container')
 const submitButton = document.getElementById('submit'); 
-const rating = document.querySelector('.button').value;
-const myRating = document.getElementById('rating'); 
-
+const rating = document.getElementById('button-rating');
+const myRating = document.getElementById('rating');
 const form = document.getElementById('buttons-container'); 
 
 
-submitButton.addEventListener('click', () => { 
 
+
+
+const inputvalue = (e) => { 
+    e.preventDefault();
     thanksContainer.classList.remove('hidden'); 
     mainContainer.style.display= "none"; 
 
-   
-})
+    let input = document.querySelectorAll('input') // Input values 
+for (let i = 0; i <input.length;i++){
+    if (input[i].checked ) { rating.innerHTML = input[i].value }} 
 
-form.addEventListener('submit', (e) => { 
-e.preventDefault(); 
+    
+}; 
 
-const data= new FormData()
+submitButton.addEventListener('click', inputvalue); 
 
-for(let value of data ){
-    console.log(value);
 
-}
 
- 
-})
 
 
 
